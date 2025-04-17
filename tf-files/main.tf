1d323a6c72228104bb860d549b8c3d846bf23c1b
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2025-04-17 05:19:05 -0500

    Update main.tf

--- tf-files/main.tf
+++ tf-files/main.tf
@@ -3,7 +3,7 @@
 resource "aws_alb_target_group" "app-lb-tg" { 
   name        = "order-table-lb-tg" 
   port        = 80    
-  protocol    = "HTTP" 
+  protocol    = "HTTP"  
   vpc_id      = data.aws_vpc.selected.id  # VPC ID where the ALB and instances reside 
   target_type = "instance"   
 
